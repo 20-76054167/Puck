81a20a508a83a1a26f5c1a833c254a79a1fc0520
swy <sungwanyoon@gmail.com>
2024-10-08 03:04:32 +0900

    modify recoil

---
+++ Source/Puck/Public/CS_RifleRecoil.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Shakes/LegacyCameraShake.h"
+#include "CS_RifleRecoil.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUCK_API URecoilCameraShake : public ULegacyCameraShake
+{
+	GENERATED_BODY()
+public:
+	URecoilCameraShake();
+};
